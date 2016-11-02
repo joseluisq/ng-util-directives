78108822d8791d888f663ff316311e042ec7296a
José Luis Quintana <joseluisquintana20@gmail.com>
2016-11-02 10:45:03 -0500

    init

---
+++ ng-util-directives.js
@@ -0,0 +1,88 @@
+(function ($, angular) {
+  angular
+
+    .module('ng-util-directives', [])
+
+    .directive('formvalidation', function () {
+      return {
+        restrict: 'A',
+        scope: {
+          fn: '&formvalidationSubmit'
+        },
+        link: function (scope, el, attrs, ngModelCtrl) {
+          $(function () {
+            var method = attrs.formvalidationSubmit || null
+
+            el.validate({
+              errorPlacement: function (error, element) {
+                $(element).parent().parent().append(error)
+              },
+              highlight: function (element, errorClass) {
+                $(element).addClass(errorClass)
+              },
+              unhighlight: function (element, errorClass) {
+                $(element).removeClass(errorClass)
+              },
+              submitHandler: function () {
+                if (method) {
+                  scope.$apply(scope.fn.apply(this, arguments))
+                }
+              }
+            })
+          })
+        }
+      }
+    })
+
+    .directive('datetimepicker', function () {
+      return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, el, attrs, ngModelCtrl) {
+          $(function () {
+            var format = attrs.datetimepickerFormat || 'DD/MM/YYYY HH:mm:ss'
+
+            el.datetimepicker({
+              locale: 'es',
+              format: format,
+              icons: {
+                time: 'fa fa-clock-o',
+                date: 'fa fa-calendar',
+                up: 'fa fa-chevron-up',
+                down: 'fa fa-chevron-down',
+                next: 'fa fa-chevron-right',
+                previous: 'fa fa-chevron-left'
+              }
+            })
+            .on('dp.change', function (e) {
+              ngModelCtrl.$setViewValue(e.date.format(format))
+              scope.$apply()
+            })
+          })
+        }
+      }
+    })
+
+    .directive('datepicker', function () {
+      return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModelCtrl) {
+          $(function () {
+            element.datepicker({
+              dateFormat: 'dd/mm/yy',
+              changeMonth: true,
+              changeYear: true,
+              minDate: new Date('1941-12-31'),
+              maxDate: new Date((new Date().getFullYear() - 18).toString() + '-12-31'),
+              yearRange: '1902:' + window.moment().format('Y'),
+              onSelect: function (date) {
+                ngModelCtrl.$setViewValue(date)
+                scope.$apply()
+              }
+            })
+          })
+        }
+      }
+    })
+})(window.jQuery, window.angular)
---
+++ readme.md
@@ -0,0 +1,66 @@
+# ng-util-directives
+> Some Angular 1.x useful directives
+
+## Install
+
+```sh
+bower install ng-util-directives --save
+```
+
+In your Angular app:
+
+```js
+angular.module('myApp', ['ng-util-directives'])
+```
+
+### [jQuery UI Datepicker](https://jqueryui.com/datepicker/) Directive
+Usage:
+
+```html
+<input type="text" class="form-control" datetimepicker datetimepicker-format="DD/MM/YYYY">
+```
+
+### [Datetimepicker](http://eonasdan.github.io/bootstrap-datetimepicker/) Directive
+
+Usage:
+
+```html
+<div class="form-group">
+  <div class="input-group date" name="txttime" name="txttime"
+    placeholder="Select your time"
+    ng-model="duration" datetimepicker datetimepicker-format="HH:mm:ss">
+
+    <input type="text" class="form-control">
+
+    <span class="input-group-addon">
+      <span class="fa fa-clock-o"></span>
+    </span>
+  </div>
+</div>
+```
+
+### [jQuery Validation](https://github.com/jzaefferer/jquery-validation) Directive
+
+Usage:
+
+```html
+<div class="container" ng-controller="AppCtrl as app">
+
+  <form formvalidation formvalidation-submit="app.onSubmit()">
+    <fieldset class="form-group">
+      <input class="form-control" id="txtemail" name="txtemail"
+        ng-model="app.email"
+        required
+        data-msg-required="Enter your e-mail"
+        data-rule-email="true"
+        data-msg-email="Enter a valid e-mail format"
+    </fieldset>
+  </form>
+
+</div>
+```
+
+## License
+MIT license
+
+© 2016 [José Luis Quintana](https://git.io/joseluisq)
